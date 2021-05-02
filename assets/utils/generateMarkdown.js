92b6fa783b3f73b087723264cd7179d2faca720d
baloyd <danielbaloy@gmail.com>
2021-05-02 16:06:36 -0400

    Fixed some comments in generateMarkdown file.

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(data.license === "Apache"){
@@ -11,7 +11,7 @@ function renderLicenseBadge(license) {
   
 }
 
-// TODO: Create a function that returns the license link
+// A function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(data.license === "Apache"){
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
   
 }
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   return`## License 
@@ -34,7 +34,7 @@ function renderLicenseSection(license) {
   ${renderLicenseLink(license)}`
 }
 
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
 function generateMarkdown(data) {
   return `#${data.projectName} 
   
